50b090d91bf198b875cb391b7678f8a26a19502f
Riswan <agyeman.riswan@gmail.com>
2020-03-04 18:06:34 -0600

    Added support for displaying other languages

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -14,30 +14,104 @@
 
 package com.google.sps.servlets;
 
+import com.google.cloud.translate.Translate;
+import com.google.cloud.translate.TranslateOptions;
+import com.google.cloud.translate.Translation;
+import com.google.sps.data.Comment;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.*;
+import com.google.gson.Gson;
+import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
 
+//ArrayList<String> commentList = new ArrayList<>();
+
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello Riswan</h1>");
-    response.setContentType("application/json");
-    PrintWriter out = response.getWriter();
-  }
-}
 
-public class InitializationExample1 {
-   public static void main(String args[]) {
-	   ArrayList<String> obj = new ArrayList<String>(
-		Arrays.asList("Google", "Software Product Sprint", "Sunnyvale"));
-	  System.out.println("Elements are:"+obj);
-   }
+    Query query = new Query("Comment").addSort("timestamp", SortDirection.DESCENDING);
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    PreparedQuery results = datastore.prepare(query);
+
+    List<String> comments = new ArrayList<>();
+
+    String languageCode = request.getParameter("languageCode");
+
+    for (Entity entity : results.asIterable()) {
+      //long id = entity.getKey().getId();
+      String text = (String) entity.getProperty("text");
+
+    // Do the translation.
+    Translate translate = TranslateOptions.getDefaultInstance().getService();
+    Translation translation =
+        translate.translate(text, Translate.TranslateOption.targetLanguage(languageCode));
+    String translatedText = translation.getTranslatedText();
+
+      comments.add(translatedText);
+    }
+    Gson gson = new Gson();
+
+    response.setCharacterEncoding("UTF-8");
+
+    response.setContentType("application/json;");
+    response.getWriter().println(gson.toJson(comments));
+
+  }
+
+  /**
+   * Converts a ServerStats instance into a JSON string using the Gson library. Note: We first added
+   * the Gson library dependency to pom.xml.
+   */
+//   private String convertToJsonUsingGson(DatastoreService commentList) {
+//     Gson gson = new Gson();
+//     String json = gson.toJson(commentList);
+//     return json;
+//   }
+
+    @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    String text = request.getParameter("text-input");
+    long timestamp = System.currentTimeMillis();
+    //String languageCode = request.getParameter("languageCode");
+
+    Entity commentEntity = new Entity("Comment");
+    commentEntity.setProperty("text", text);
+    commentEntity.setProperty("timestamp", timestamp);
+   // commentEntity.setParameter("languageCode", languageCode);
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    datastore.put(commentEntity);
+
+    //response.setContentType("application/json;");
+    //response.getWriter().println(json);
+
+    response.sendRedirect("/index.html");
+  }
+
+  /**
+   * @return the request parameter, or the default value if the parameter
+   *         was not specified by the client
+   */
+  private String getParameter(HttpServletRequest request, String name, String defaultValue) {
+    String value = request.getParameter(name);
+    if (value == null) {
+      return defaultValue;
+    }
+    return value;
+  }
 }
